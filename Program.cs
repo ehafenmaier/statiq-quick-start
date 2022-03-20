ec3907ac3b91c1bc42d25b82e43f2d71c9454ab7
Erich Hafenmaier <ehafenmaier@gmail.com>
2022-03-20 10:30:59 -0600

    Add bootstapper and index markdown page

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Threading.Tasks;
+using Statiq.App;
+using Statiq.Web;
+
+public class Program
+{
+    public static async Task<int> Main(string[] args) =>
+        await Bootstrapper
+            .Factory
+            .CreateWeb(args)
+            .RunAsync();
+}
